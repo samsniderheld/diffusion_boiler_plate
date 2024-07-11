cfab94976a1a6f926408eb75520a8091a27a2079
samsniderheld <samsniderheld@gmail.com>
2024-07-11 16:44:57 -0400

    Update lora_inpainting_controlnet_pipelines.py

--- pipelines/lora_inpainting_controlnet_pipelines.py
+++ pipelines/lora_inpainting_controlnet_pipelines.py
@@ -15,7 +15,7 @@ import torch
 
 from compel import Compel, ReturnedEmbeddingsType
 from controlnet_aux.processor import Processor
-from diffusers import DPMSolverMultistepScheduler, StableDiffusionXLControlNetInpaintPipeline
+from diffusers import DPMSolverMultistepScheduler, StableDiffusionXLControlNetInpaintPipeline, StableDiffusionControlNetInpaintPipeline,DEISMultistepScheduler
 from diffusers.models import ControlNetModel, AutoencoderKL
 from diffusers import AutoPipelineForImage2Image
 from diffusers.utils import load_image
@@ -44,14 +44,14 @@ class SDXL_Pipeline():
                 controlnet = ControlNetModel.from_pretrained(path, torch_dtype=torch.float16)
                 self.controlnets.append(controlnet)
 
-        vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16)
+        # vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16)
 
         #load pipeline
         self.pipeline = StableDiffusionXLControlNetInpaintPipeline.from_pretrained(
             self.base_pipeline_path, 
             controlnet=self.controlnets, 
             torch_dtype=torch.float16,
-            vae=vae,
+            # vae=vae,
         )
 
         self.pipeline.scheduler =  DPMSolverMultistepScheduler.from_config(
@@ -59,7 +59,7 @@ class SDXL_Pipeline():
             use_karras=True, 
             euler_at_final=True,
             rescale_betas_zero_snr=True, 
-            vae=vae,
+            # vae=vae,
         )
 
         # self.pipeline.enable_vae_tiling()
@@ -101,14 +101,15 @@ class SDXL_Pipeline():
       gc.collect()
       torch.cuda.empty_cache()
 
-    def generate_img(self, prompt,negative_prompt, bg_image, mask_image,controlnet_image_path, controlnet_scale, control_guidance_start, control_guidance_end, lora_weights, cfg, steps, seed=None, width=1024, height=1024):
+    def generate_img(self, prompt,negative_prompt, bg_image_path, mask_image_path,controlnet_image_path, controlnet_scale, control_guidance_start, control_guidance_end, lora_weights, cfg, steps, seed=None, width=1024, height=1024):
         """generates an image based on the given prompts and control parameters"""
         # Check that the sizes of the controlnets, images, and controlnet_scale match
         if len(self.controlnets) != len(controlnet_scale):
             raise ValueError("The sizes of the controlnets, images, and controlnet_scale must match")
         
-        if len(self.additional_loras) != len(lora_weights):
-            raise ValueError("The sizes of the additional_loras and lora_weights must match")
+        if(self.additional_loras):
+          if len(self.additional_loras) != len(lora_weights):
+              raise ValueError("The sizes of the additional_loras and lora_weights must match")
 
         #deterministic seed
         if seed is None:
@@ -116,6 +117,8 @@ class SDXL_Pipeline():
 
         generator = torch.Generator(device='cuda').manual_seed(seed)
         controlnet_image = load_image(controlnet_image_path)
+        bg_image = load_image(bg_image_path)
+        mask_image = load_image(mask_image_path)
         images = []
 
         # prepare controlnet images
@@ -127,8 +130,9 @@ class SDXL_Pipeline():
                 images.append(processed_image)
 
                 
-        if (self.additional_loras) :
-            self.pipeline.set_adapters([lora['model_id'] for lora in self.additional_loras], adapter_weights=lora_weights)
+        if(self.additional_loras):
+          if len(self.additional_loras) != len(lora_weights):
+              raise ValueError("The sizes of the additional_loras and lora_weights must match")
 
         conditioning, pooled = self.compel(prompt)
 
@@ -142,6 +146,189 @@ class SDXL_Pipeline():
             pooled_prompt_embeds=pooled,
             negative_prompt_embeds=negative_conditioning,
             negative_pooled_prompt_embeds=negative_pooled,
+            num_inference_steps=steps,
+            image=bg_image,
+            mask_image=mask_image,
+            control_image=images,
+            generator=generator,
+            content_guidance_scale=cfg,
+            controlnet_conditioning_scale=controlnet_scale,
+            control_guidance_start = control_guidance_start,
+            control_guidance_end = control_guidance_end,
+            width=width,
+            height=height
+        ).images[0]
+
+        end_time = time.time()
+
+        elapsed_time = end_time - start_time
+
+        print(f"The script took {elapsed_time:.2f} seconds to execute.")
+        
+        params = self.print_parameters(prompt, negative_prompt, controlnet_image_path, width, height, controlnet_scale, control_guidance_start, control_guidance_end, lora_weights, cfg, steps, seed=seed)
+
+        image.info['comment'] = params
+
+        return image, params, images
+    
+    
+    def refine_image(self,prompt,negative_prompt, image, cfg, steps, strength=.15, seed=None):
+        """refines an image using the refiner model"""
+        if not self.use_refiner:
+            raise ValueError("The refiner is not enabled")
+        
+        #deterministic seed
+        if seed is None:
+            seed = random.randint(0,10000)
+
+        generator = torch.Generator(device='cuda').manual_seed(seed)
+
+        start_time = time.time()
+
+        image = self.ref_pipeline(
+            prompt,
+            negative_prompt=negative_prompt,
+            image=image,
+            strength=strength,
+            generator=generator,
+            content_guidance_scale=cfg,
+            num_inference_steps=steps,
+        ).images[0]
+
+        end_time = time.time()
+        
+        elapsed_time = end_time - start_time
+
+        print(f"The script took {elapsed_time:.2f} seconds to execute.")
+
+        return image
+
+
+    def print_parameters(self, prompt, negative_prompt, controlnet_image_path, width, height, controlnet_scale, control_guidance_start, control_guidance_end, lora_weights, cfg, steps, seed=None):
+        """a function to collate all parameters into a json string for easy saving and loading of parameters"""
+        parameters = {
+            "base_pipeline_path": self.base_pipeline_path,
+            "controlnet_paths": self.additional_controlnet_paths,
+            "loras": self.additional_loras,
+            "input_image": controlnet_image_path,
+            "width": width,
+            "height": height,
+            "prompt": prompt,
+            "negative_prompt": negative_prompt,
+            "controlnet_scale": controlnet_scale,
+            "control_guidance_start": control_guidance_start,
+            "control_guidance_end": control_guidance_end,
+            "lora_weights": lora_weights,
+            "cfg": cfg,
+            "steps": steps,
+            "seed": seed,
+            "refiner_str":.15
+        }
+
+        return json.dumps(parameters, indent=4)
+    
+
+
+class SD15_Pipeline():
+    """class to house all the pipeline loading and generation functions for easy looping and iteration"""
+    def __init__(self, base_pipeline_path, additional_controlnet_paths=None, additional_loras=None, use_distributed=False):
+        self.base_pipeline_path = base_pipeline_path
+        self.additional_controlnet_paths = additional_controlnet_paths
+        self.additional_loras = additional_loras
+        self.controlnet_preprocessors = {
+
+            "lllyasviel/sd-controlnet-depth": "depth_midas",
+            "lllyasviel/sd-controlnet-canny": "canny"
+        }
+        self.use_distributed = use_distributed
+        self.controlnets = []
+
+    def load_pipeline(self):
+        """loads all the necessary components for the pipeline to function"""
+        if(self.additional_controlnet_paths):
+            for path in self.additional_controlnet_paths:
+                controlnet = ControlNetModel.from_pretrained(path, torch_dtype=torch.float16)
+                self.controlnets.append(controlnet)
+
+        # vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16)
+
+        #load pipeline
+        self.pipeline = StableDiffusionControlNetInpaintPipeline.from_pretrained(
+            self.base_pipeline_path, 
+            controlnet=self.controlnets, 
+            torch_dtype=torch.float16,
+            # vae=vae,
+        )
+
+        self.pipeline.scheduler =  DEISMultistepScheduler.from_config(self.pipeline.scheduler.config)
+
+        # self.pipeline.enable_vae_tiling()
+
+        if self.use_distributed:
+            self.pipeline.to("cuda:0")
+        else:
+            self.pipeline.to("cuda")
+
+        if(self.additional_loras):
+            for lora in self.additional_loras:
+                lora_model_id = lora['model_id']
+                lora_filename = lora['filename']
+                self.pipeline.load_lora_weights(lora_model_id, weight_name=lora_filename, adapter_name=lora_model_id)
+
+        self.compel = Compel(
+            tokenizer=self.pipeline.tokenizer ,
+            text_encoder=self.pipeline.text_encoder,
+            truncate_long_prompts=True
+        )
+
+
+    def flush(self):
+      gc.collect()
+      torch.cuda.empty_cache()
+
+    def generate_img(self, prompt,negative_prompt, bg_image_path, mask_image_path,controlnet_image_path, controlnet_scale, control_guidance_start, control_guidance_end, lora_weights, cfg, steps, seed=None, width=1024, height=1024):
+        """generates an image based on the given prompts and control parameters"""
+        # Check that the sizes of the controlnets, images, and controlnet_scale match
+        if len(self.controlnets) != len(controlnet_scale):
+            raise ValueError("The sizes of the controlnets, images, and controlnet_scale must match")
+        
+        if(self.additional_loras):
+          if len(self.additional_loras) != len(lora_weights):
+              raise ValueError("The sizes of the additional_loras and lora_weights must match")
+
+        #deterministic seed
+        if seed is None:
+            seed = random.randint(0,10000)
+
+        generator = torch.Generator(device='cuda').manual_seed(seed)
+        controlnet_image = load_image(controlnet_image_path)
+        bg_image = load_image(bg_image_path)
+        mask_image = load_image(mask_image_path)
+        images = []
+
+        # prepare controlnet images
+        if(self.additional_controlnet_paths):
+            for path in self.additional_controlnet_paths:
+                processor_id = self.controlnet_preprocessors[path]
+                processor = Processor(processor_id)
+                processed_image = processor(controlnet_image, to_pil=True).resize((width,height))
+                images.append(processed_image)
+
+                
+        if(self.additional_loras):
+          if len(self.additional_loras) != len(lora_weights):
+              raise ValueError("The sizes of the additional_loras and lora_weights must match")
+
+        conditioning = self.compel(prompt)
+
+        negative_conditioning = self.compel(negative_prompt)
+
+        # generate image
+        start_time = time.time()
+
+        image = self.pipeline(
+            prompt_embeds=conditioning,
+            negative_prompt_embeds=negative_conditioning,
             num_inference_steps=steps,
             image=bg_image,
             mask_image=mask_image,
